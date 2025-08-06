36dd69b9d4524e9cd66e6cb277f5af39500ce330
ngure1 <ngugijoseph700@gmail.com>
2025-08-06 11:28:23 +0300

    setup env

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -4,19 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/ngure1/hackatsuki/pkg/grpcserver"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":8080")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading env %s \n", err)
+	}
+
+	port := os.Getenv("PORT")
+	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatalf("Error starting the server %s \n", err)
 	}
 
 	server := grpcserver.NewGrpcServer()
 
-	fmt.Printf("Server Running on port 8080...\n")
+	fmt.Printf("Server Running on port %s...\n", port)
 	err = server.Serve(listener)
 	if err != nil {
 		log.Fatalf("Error starting the grpc server %s \n", err)
