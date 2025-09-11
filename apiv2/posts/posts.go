a9a3762e6b94074d20b694dc57ad3fd3571c5b36
ngure1 <ngugijoseph700@gmail.com>
2025-09-11 10:40:31 +0300

    refactor: :recycle: improve typing on post store interface

--- apiv2/posts/posts.go
+++ apiv2/posts/posts.go
@@ -4,9 +4,10 @@ import "apiv2/internal/models"
 
 type Store interface {
 	// page limit
-	GetPosts(int, int) ([]models.Post, int, error)
-	GetPost(uint) (*models.Post, error)
+	GetPosts(page int, limit int) ([]models.Post, int, error)
+	GetPost(postId uint) (*models.Post, error)
 
-	CreatePost(string, string, uint, *string, *string) (*models.Post, error)
-	LikePost(uint, uint) error
+	CreatePost(question string, description string, userId uint, crop *string, imageUrl *string) (*models.Post, error)
+	LikePost(postId uint, userId uint) error
+	CreateComment(content string, postId uint, userId uint, parentCommentId *uint) (*models.Comment, error)
 }
